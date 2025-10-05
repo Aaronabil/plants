b6e0fd4b99845384d8e5c202f90855d5e66462d4
Aaronabil <antomi.tomi1979@gmail.com>
2025-10-05 14:23:48 +0700

    add table products

---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'category_id',
+        'product_name',
+        'description',
+        'price',
+        'stock',
+        'weight_in_kilograms',
+    ];
+}
