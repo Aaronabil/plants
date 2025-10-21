3f7ff84691f982356a1020c88064eeec82f28710
alyssour <alyssanrlh@gmail.com>
2025-10-21 13:58:39 +0700

    add import ui

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,22 +4,28 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'sonner'; 
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.tsx`,
-            import.meta.glob('./Pages/**/*.tsx'),
-        ),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) =>
+    resolvePageComponent(
+      `./Pages/${name}.tsx`,
+      import.meta.glob('./Pages/**/*.tsx'),
+    ),
+  setup({ el, App, props }) {
+    const root = createRoot(el);
 
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#00c951',
-    },
+    root.render(
+      <>
+        <App {...props} />
+        <Toaster richColors position="top-center" />
+      </>
+    );
+  },
+  progress: {
+    color: '#00c951',
+  },
 });
