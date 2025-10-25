d48ec1cbae53f99e7ba2cfcdc7c47da661f41d6c
Aaronabil <antomi.tomi1979@gmail.com>
2025-10-25 12:55:51 +0700

    add feature related product sesuai indoor atau outdoornya plant

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,9 +11,21 @@ class ProductController extends Controller
     public function show(Product $product)
     {
         $product->load('images', 'category.parent');
-        
+
+        $rootCategory = $product->category->parent ?? $product->category;
+
+        $relatedProducts = Product::whereHas('category', function ($query) use ($rootCategory) {
+            $query->where('parent_id', $rootCategory->id);
+        })
+            ->where('id', '!=', $product->id)
+            ->with(['primaryImage', 'category'])
+            ->inRandomOrder()
+            ->limit(20)
+            ->get();
+
         return Inertia::render('Product/Show', [
-            'product' => $product
+            'product' => $product,
+            'relatedProducts' => $relatedProducts
         ]);
     }
 }
