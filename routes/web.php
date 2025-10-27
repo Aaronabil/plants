8abb013e151330c3c72fb095e04bb82d3144f700
Aaronabil <antomi.tomi1979@gmail.com>
2025-10-27 15:46:07 +0700

    comment dashboard

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ use Inertia\Inertia;
 
 // Route User
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
