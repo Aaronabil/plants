0820c14f86da219580a164a4509c1f691da4d3a1
Aaronabil <antomi.tomi1979@gmail.com>
2025-10-28 00:46:02 -0300

    depedencies

--- resources/js/Pages/Admin/Login.tsx
+++ resources/js/Pages/Admin/Login.tsx
@@ -1,9 +1,22 @@
 import { useEffect, FormEventHandler } from 'react';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
+import { GalleryVerticalEnd } from "lucide-react"
+import InputError from "@/Components/InputError"
+import { Button } from "@/Components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/Components/ui/card"
+import {
+    Field,
+    FieldDescription,
+    FieldGroup,
+    FieldLabel,
+    FieldSeparator,
+} from "@/Components/ui/field"
+import { Input } from "@/Components/ui/input"
 import { Head, Link, useForm } from '@inertiajs/react';
 
 export default function Login({ status, canResetPassword }: { status?: string, canResetPassword?: boolean }) {
@@ -31,7 +44,79 @@ export default function Login({ status, canResetPassword }: { status?: string, c
 
             {status && <div className="mb-4 text-sm font-medium text-green-600">{status}</div>}
 
-            <form onSubmit={submit}>
+            <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+                <div className="flex w-full max-w-sm flex-col gap-6">
+                    <a href="#" className="flex items-center gap-2 self-center font-medium">
+                        <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+                            <GalleryVerticalEnd className="size-4" />
+                        </div>
+                        Yestera Admin
+                    </a>
+                    <div className="flex flex-col gap-6">
+                        <Card>
+                            <CardHeader className="text-center">
+                                <CardTitle className="text-xl">Welcome back</CardTitle>
+                                <CardDescription>
+                                    Log in to your admin account to manage your site
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <form onSubmit={submit}>
+                                    <FieldGroup>
+                                        <Field>
+                                            <FieldLabel htmlFor="email">Email</FieldLabel>
+                                            <Input
+                                                id="email"
+                                                type="email"
+                                                name="email"
+                                                value={data.email}
+                                                className="block w-full mt-1"
+                                                autoComplete="username"
+                                                placeholder="admin@example.com"
+                                                required
+                                                onChange={(e) => setData('email', e.target.value)}
+                                            />
+                                            <InputError message={errors.email} className="mt-2" />
+                                        </Field>
+                                        <Field>
+                                            <div className="flex items-center">
+                                                <FieldLabel htmlFor="password">Password</FieldLabel>
+                                                <a
+                                                    href="#"
+                                                    className="ml-auto text-sm underline-offset-4 hover:underline"
+                                                >
+                                                    Forgot your password?
+                                                </a>
+                                            </div>
+                                            <Input
+                                                id="password"
+                                                type="password"
+                                                name="password"
+                                                value={data.password}
+                                                className="block w-full mt-1"
+                                                autoComplete="current-password"
+                                                placeholder="Enter your password"
+                                                required
+                                                onChange={(e) => setData('password', e.target.value)}
+                                            />
+                                            <InputError message={errors.email} className="mt-2" />
+                                        </Field>
+                                        <Field>
+                                            <Button type="submit" disabled={processing}>Login</Button>
+                                        </Field>
+                                    </FieldGroup>
+                                </form>
+                            </CardContent>
+                        </Card>
+                        <FieldDescription className="px-6 text-center">
+                            By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+                            and <a href="#">Privacy Policy</a>.
+                        </FieldDescription>
+                    </div>
+                </div>
+            </div>
+
+            {/* <form onSubmit={submit}>
                 <div>
                     <InputLabel htmlFor="email" value="Email" />
 
@@ -70,7 +155,7 @@ export default function Login({ status, canResetPassword }: { status?: string, c
                         Log in
                     </PrimaryButton>
                 </div>
-            </form>
+            </form> */}
         </>
     );
 }
