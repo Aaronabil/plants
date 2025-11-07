bd99c49ad491a1d923ebb985ec098dafe6cc213e
Aaronabil <antomi.tomi1979@gmail.com>
2025-11-07 23:10:29 +0700

    function customer
    function max 10 data per page, add phone and created_at at interface User

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class CustomerController extends Controller
+{
+    public function index(Request $request)
+    {
+        $perPage = $request->input('per_page', 10);
+        $customers = User::orderBy('id', 'ASC')->paginate($perPage);
+
+        return Inertia::render('Admin/Customer/Page', [
+            'customers' => $customers,
+        ]);
+    }
+}
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -39,6 +39,8 @@ export interface User {
     id: number;
     name: string;
     email: string;
+    phone?: string;
+    created_at: string;
     email_verified_at?: string;
 }
 
